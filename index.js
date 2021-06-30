ec40901b201644bb72b847eaa4c98080cdc55887
github username <github email address>
2021-06-30 17:45:41 -0400

    commit tree fd0779abc430fad74b42428fdb5aab7a9928f804

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    const newDrivers = drivers.filter(function (drivers) { return drivers.toLowerCase() === name.toLowerCase() })
+    return newDrivers;
+}
+
+function fuzzyMatch(drivers, string) {
+    const letterDrivers = drivers.filter(function (drivers) { return drivers[0] === string[0] })
+    return letterDrivers;
+
+}
+
+function matchName(drivers, name){
+    return drivers.filter(function(driver){
+      return driver.name === name
+    })
+  }
